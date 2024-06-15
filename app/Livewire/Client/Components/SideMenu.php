688226fe196f2368de10d661fa5c0ba8f8920cbf
Azarangit <azaranvalvegit@gmail.com>
2024-06-15 14:35:46 +0330

    menu

---
+++ app/Livewire/Client/Components/SideMenu.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Client\Components;
+
+use Livewire\Component;
+
+class SideMenu extends Component
+{
+    public function render()
+    {
+        return view('livewire.client.components.side-menu');
+    }
+}
--- resources/views/livewire/client/layouts/menu.blade.php
+++ resources/views/livewire/client/layouts/menu.blade.php
@@ -1,60 +1,114 @@
+<?php
+$menus = [
+    [
+        "title" => 'صفحه نخست',
+        "href" => '/',
+        "subMenus" => []
+    ],
+    [
+        "title" => 'خدمات سایت',
+        "href" => 'dfdfsf',
+        "subMenus" => [
+            [
+                "title" => 'طراحی سایت ارزان',
+                "href" => '/fdfdd',
+                "subMenus" => []
+            ],
+            [
+                "title" => 'طراحی سایت اختصاصی',
+                "href" => 'sdfdfsd',
+                "subMenus" => []
+            ],
+            [
+                "title" => 'طراحی نرم افزار تحت وب و موبایل',
+                "href" => 'dsfsdfs',
+                "subMenus" => []
+            ],
+            [
+                "title" => 'سئو و تولید محتوا',
+                "href" => 'sdfsdsd',
+                "subMenus" => []
+            ],
+            [
+                "title" => 'طراحی قالب و افزونه وردپرس اختصاصی',
+                "href" => 'sdfdsfsdf',
+                "subMenus" => []
+            ],
+        ]
+    ],
+    [
+        "title" => 'ادمین اینستاگرام',
+        "href" => '',
+        "subMenus" => [
+            [
+                "title" => 'مدیریت کامل پیج',
+                "href" => '',
+                "subMenus" => []
+            ],
+            [
+                "title" => 'طراحی محصول',
+                "href" => '',
+                "subMenus" => []
+            ],
+            [
+                "title" => 'کامنت مارکتینگ',
+                "href" => '',
+                "subMenus" => []
+            ],
+            [
+                "title" => 'فروش و کمپین',
+                "href" => '',
+                "subMenus" => []
+            ],
+            [
+                "title" => 'آنالیز پیج و مشاوره',
+                "href" => '',
+                "subMenus" => []
+            ],
+        ]
+    ],
+    [
+        "title" => 'گرافیک و تدوین',
+        "href" => '',
+        "subMenus" => [
+            [
+                "title" => 'ادیت ویدئو',
+                "href" => '',
+                "subMenus" => []
+            ],
+            [
+                "title" => 'طراحی لوگو',
+                "href" => '',
+                "subMenus" => []
+            ],
+        ]
+    ],
+    [
+        "title" => 'همکاری',
+        "href" => '',
+        "subMenus" => []
+    ],
+    [
+        "title" => 'تماس با ما',
+        "href" => '',
+        "subMenus" => []
+    ],
+];
+?>
+
 <ul class="mainmenu">
-    <li class="menu-item-has-children">
-        <a href="javascript:void(0);">Digital Agency</a>
-        <ul class="axil-submenu">
-            <li><a href="index-1.html">Digital Agency</a></li>
-            <li><a href="index-2.html">Creative Agency</a></li>
-            <li><a href="index-3.html">Personal Portfolio</a></li>
-            <li><a href="index-4.html">Home Startup</a></li>
-            <li><a href="index-5.html">Corporate Agency</a></li>
-        </ul>
-    </li>
-    <li class="menu-item-has-children">
-        <a href="javascript:void(0);">Services</a>
-        <ul class="axil-submenu">
-            <li><a href="service.html">Service</a></li>
-            <li><a href="service-2.html">Service Two</a></li>
-            <li><a href="service-design.html">Service Details</a></li>
-        </ul>
-    </li>
-    <li class="menu-item-has-children">
-        <a href="javascript:void(0);">Portfolio</a>
-        <ul class="axil-submenu">
-            <li><a href="portfolio.html">Two Column</a></li>
-            <li><a href="portfolio-2.html">Three Column</a></li>
-            <li><a href="portfolio-3.html">Four Column</a></li>
-            <li><a href="portfolio-4.html">Three Column Width</a></li>
-            <li><a href="portfolio-5.html">Four Column Width</a></li>
-            <li><a href="single-portfolio.html">Portfolio Details</a></li>
-        </ul>
-    </li>
-    <li class="menu-item-has-children">
-        <a href="javascript:void(0);">Pages</a>
-        <ul class="axil-submenu">
-            <li><a href="about-us.html">About Us</a></li>
-            <li><a href="our-office.html">Our Office</a></li>
-            <li><a href="case-study.html">Case Study</a></li>
-            <li><a href="single-case-study.html">Case Study Details</a></li>
-            <li><a href="team.html">Team</a></li>
-            <li><a href="single-team.html">Team Details</a></li>
-            <li><a href="our-clients.html">Our Clients</a></li>
-            <li><a href="testimonial.html">Testimonial</a></li>
-            <li><a href="pricing-table.html">Pricing Table</a></li>
-            <li><a href="typography.html">Typography</a></li>
-            <li><a href="404.html">404 Page</a></li>
-            <li><a href="coming-soon.html">Coming Soon</a></li>
-        </ul>
-    </li>
-    <li class="menu-item-has-children">
-        <a href="javascript:void(0);">Blog</a>
-        <ul class="axil-submenu">
-            <li><a href="blog.html">Blog</a></li>
-            <li><a href="single-blog.html">Standard Post</a></li>
-            <li><a href="single-blog-gallery.html">Gallery Post</a></li>
-            <li><a href="single-blog-video.html">Video Post</a></li>
-            <li><a href="single-blog-audio.html">Audio Post</a></li>
-            <li><a href="single-blog-quote.html">Quote Post</a></li>
-        </ul>
-    </li>
-    <li><a href="contact.html">Contact</a></li>
+    @foreach($menus as $menu)
+        <li class="{{ sizeof($menu['subMenus']) > 0 ? 'menu-item-has-children' : '' }}">
+            <a href="{{ sizeof($menu['subMenus'])==0 ? $menu['href'] : 'javascript:void(0);' }} ">{{$menu['title']}}</a>
+            @if(sizeof($menu['subMenus']) > 0)
+                <ul class="axil-submenu">
+                    @foreach($menu['subMenus'] as $subMenu)
+                        <li>
+                            <a href="{{$subMenu['href']}}">{{$subMenu['title']}}</a>
+                        </li>
+                    @endforeach
+                </ul>
+            @endif
+        </li>
+    @endforeach
 </ul>
