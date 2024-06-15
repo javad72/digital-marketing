795e4892d278678999f850e38b667b9544831f79
جواد <150221236+pro-3223@users.noreply.github.com>
2024-06-16 00:19:07 +0330

    side menu

--- resources/views/livewire/client/components/side-menu.blade.php
+++ resources/views/livewire/client/components/side-menu.blade.php
@@ -9,48 +9,49 @@
     <div class="offcanvas-body">
         <form action="#" class="side-nav-search-form">
             <div class="form-group">
-                <input type="text" class="search-field" name="search-field" placeholder="Search...">
+                <input type="text" class="search-field" name="search-field" placeholder="جست و جو">
                 <button class="side-nav-search-btn"><i class="fas fa-search"></i></button>
             </div>
         </form>
         <div class="row ">
-            <div class="col-lg-5 col-xl-6">
-                <ul class="main-navigation list-unstyled">
-                    <li><a href="index-1.html">Digital Agency</a></li>
-                    <li><a href="index-2.html">Creative Agency</a></li>
-                    <li><a href="index-3.html">Personal Portfolio</a></li>
-                    <li><a href="index-4.html">Home Startup</a></li>
-                    <li><a href="index-5.html">Corporate Agency</a></li>
-                </ul>
-            </div>
             <div class="col-lg-7 col-xl-6">
                 <div class="contact-info-wrap">
                     <div class="contact-inner">
                         <address class="address">
-                            <span class="title">Contact Information</span>
-                            <p>Theodore Lowe, Ap #867-859 <br> Sit Rd, Azusa New York</p>
+                            <h6 class="title">اطلاعات تماس</h6>
+                            <p> ما همیشه آنلاین هستیم پس همین حالا تماس بگیرید!</p>
                         </address>
                         <address class="address">
-                            <span class="title">We're Available 24/7. Call Now.</span>
-                            <a class="tel" href="tel:8884562790"><i class="fas fa-phone"></i>(888)
-                                456-2790</a>
-                            <a class="tel" href="tel:12125553333"><i class="fas fa-fax"></i>(121)
-                                255-53333</a>
+
+                            <a class="tel" href="tel:09138686326"><i class="fas fa-phone ms-2 me-0"></i>
+                                09138686326</a>
+                            <a class="tel" href="tel:09136549432"><i class="fas fa-phone ms-2 me-0"></i>
+                                09136549432</a>
                         </address>
                     </div>
                     <div class="contact-inner">
-                        <h5 class="title">Find us here</h5>
+                        <h5 class="title"><strong>تجارت نو</strong> در شبکه های اجتماعی</h5>
                         <div class="contact-social-share">
                             <ul class="social-share list-unstyled">
-                                <li><a href="https://facebook.com/"><i class="fab fa-facebook-f"></i></a></li>
-                                <li><a href="https://twitter.com/"><i class="fab fa-twitter"></i></a></li>
-                                <li><a href="https://www.behance.net/"><i class="fab fa-behance"></i></a></li>
+                                <li><a href="https://facebook.com/"><i class="fab fa-telegram-plane"></i></a></li>
+                                <li><a href="https://twitter.com/"><i class="fab fa-whatsapp"></i></a></li>
+                                <li><a href="https://www.behance.net/"><i class="fab fa-instagram"></i></a></li>
                                 <li><a href="https://www.linkedin.com/"><i class="fab fa-linkedin-in"></i></a></li>
                             </ul>
                         </div>
                     </div>
                 </div>
             </div>
+            <div class="col-lg-5 col-xl-6">
+                <ul class="main-navigation list-unstyled">
+                    <li><a href="index-1.html">چرا کسب و کار آنلاین مهم است؟</a></li>
+                    <li><a href="index-2.html">تضمین موفقیت در آنلاین شاپ</a></li>
+                    <li><a href="index-3.html">Personal Portfolio</a></li>
+                    <li><a href="index-4.html">Home Startup</a></li>
+                    <li><a href="index-5.html">Corporate Agency</a></li>
+                </ul>
+            </div>
+
         </div>
     </div>
 </div>
